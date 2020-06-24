6ba4082c372631393ff01b6a3011ece461abac40
Yuri Melo <yurifullstack@gmail.com>
2020-06-24 05:40:41 -0300

    Setting up timezone

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const connection = new Sequelize('ocean', 'root', 'yuriMELO123', {
 	host: 'localhost',
-	dialect: 'mysql'
+	dialect: 'mysql',
+	timezone: "-03:00"
 });
 
 module.exports = connection;
