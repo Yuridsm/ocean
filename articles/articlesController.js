2b6be520ebcdd63e7074f4e9acbb9d13e60983a2
Yuri Melo <yurifullstack@gmail.com>
2020-07-01 12:29:26 -0300

    Configuring pagination

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -74,4 +74,62 @@ router.get("/admin/articles/edit/:id", (req, res) => {
 	});
 });
 
+router.post("/articles/update", (req, res) => {
+	let id = req.body.id;
+	let title = req.body.title;
+	let body = req.body.body;
+	let category = req.body.category;
+	Article.update({
+		title: title, 
+		body: body, 
+		categoryId: category, 
+		slug: slugify(title) 
+	}, { where: {id: id} }).then(() => {
+		res.redirect("/admin/articles");
+	}).catch(err => {
+		res.redirect("/");
+	});
+
+});
+
+
+router.get("/articles/page/:num", (req, res) => {
+	let page = req.params.num;
+	let offset = 0;
+	if(isNaN(page) || page <= 1) {
+		offset = 0;
+	} else {
+		offset = (parseInt(page)*4) -4;
+	}
+
+	Article.findAndCountAll({
+		limit: 4,
+		offset: offset
+	}).then(articles => {
+		let next;
+
+		/**
+		 We are verifying whether the amount of last page plus 4 is more than total
+		 amount of articles.
+		*/
+		if(offset + 4 >= articles.count) {
+			next = false;
+		} else {
+			next = true;
+		}
+
+		let result = {
+			next: next,
+			articles: articles
+		}
+
+		Category.findAll().then(categories => {
+			res.render("admin/articles/page", {
+				result: result,
+				categories: categories
+			});
+		});
+
+	});
+});
 module.exports = router;
